832704853e2ecaad948b95e4de13ed04e919b987
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-09-16 19:40:22 +0300

    Sample using where

--- LinqExercises/Program.cs
+++ LinqExercises/Program.cs
@@ -8,6 +8,7 @@ namespace LinqExercises
         static void Main(string[] args)
         {
 			
+
 		}
 
 		private static void Sample1_SelectEvenNumbers()
@@ -31,6 +32,31 @@ namespace LinqExercises
 				{
 					break;
 				}
+			}
+		}
+		private static void FiletrOperator_Where_PersonOver14StartingWithM()
+        {
+			//var query = from person in PersonsDatabase.AllPersons()
+			//			where person.Age >= 14 && person.LastName.StartsWith('M')
+			//			select person;
+
+			var query = PersonsDatabase.AllPersons()
+							.Where(p => p.Age >= 14)
+							.Where(p => p.LastName.Contains('M'));
+
+			foreach (var person in PersonsDatabase.AllPersons())
+			{
+				person.Print();
+			}
+
+			Console.WriteLine("______________________________________________________________");
+			Console.WriteLine("Query result: ");
+			Console.WriteLine("______________________________________________________________");
+
+
+			foreach (var person in query)
+			{
+				person.Print();
 			}
 		}
     }
